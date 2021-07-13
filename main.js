dfc9055b1ae74fb74d13d29c3fe4ee85f94ce3ea
JG <joshgundy55@gmail.com>
2021-07-13 16:16:17 -0500

    Add next batch of tests

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import capitalize from './capitalize';
 import reverseString from './reverseString';
 import calculator from './calculator';
-
 test('capitalize: first letter capitalized', () => {
   expect(capitalize('Letter')).toMatch(/^[A-Z]/);
 });
@@ -21,7 +20,7 @@ test('reverseString: capitalization is preserved', () => {
 test('reverseString: sentences are properly reversed', () => {
   expect(reverseString('Mary had a little lamb'))
     .toBe('bmal elttil a dah yraM');
-})
+});
 
 test('calculator: contains +, -, x, /', () => {
   expect(calculator).toEqual(expect.objectContaining({
@@ -46,4 +45,48 @@ test('calculator: multiply multiplies', () => {
 
 test('calculator: divide divides', () => {
   expect(calculator.divide(8, 4)).toEqual(2);
+});
+
+test('caesar: shifts string', () => {
+  expect(caesar('mary')).toEqual('pdub');
+});
+
+test('caesar: handles punctuation', () => {
+  expect(caesar('/,gk')).toEqual(',/jn');
+});
+
+test('caesar: preserves case', () => {
+  expect(caesar('mArY')).toEqual('pDuB');
+});
+
+test('caesar: handles sentences', () => {
+  expect(caesar('Mary had a little lamb, and that was great for her. Mary had a friend, too! Her name was Sarah'))
+    .toEqual('Pdub kdg d olwwoh odpe, dqg wkdw zdv juhdw iru khu. Pdub kdg d iulhqg, wrr! Khu qdph zdv Vdudk');
+});
+
+test('analyze: contains right properties', () => {
+  expect(analyze([1,2,3,4,5,6])).toEqual(expect.objectContaining({
+    average: expect.any(Number);
+    min: expect.any(Number);
+    max: expect.any(Number);
+    length: expect.any(Number);
+  }));
+});
+
+const array = analyze([1,2,3,4,5,6]);
+
+test('analyze: average is the average', () => {
+  expect(array.average).toEqual(3.5);
+});
+
+test('analyze: min is the min', () => {
+  expect(array.min).toEqual(1);
+});
+
+test('analyze: max is the max', () => {
+  expect(array.max).toEqual(6);
+});
+
+test('analyze: length is the length', () => {
+  expect(array.length).toEqual(6);
 });
