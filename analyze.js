c3afdbbd6ac52a614ebc0ecc3a8f55d897dc08af
JG <joshgundy55@gmail.com>
2021-07-13 18:14:29 -0500

    All tests passing

--- analyze.js
+++ analyze.js
@@ -1,4 +1,9 @@
-const analyze = () => {
-
-}
+const analyze = (array) => {
+  const results = {};
+  results.average = Math.round((array.reduce((prev, ele) => prev + ele) / array.length) * 10) / 10;
+  results.min = array.reduce((prev, ele) => ((prev < ele) ? prev : ele));
+  results.max = array.reduce((prev, ele) => ((prev > ele) ? prev : ele));
+  results.length = array.length;
+  return results;
+};
 export default analyze;
